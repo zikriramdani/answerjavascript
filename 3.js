e94a159a3cf77dfe8a919564d6f46ea41c987290
unknown <zikriramdani@gmail.com>
2019-11-30 21:39:48 +0700

    M

--- 3.js
+++ 3.js
@@ -15,42 +15,46 @@ function isEmirp(n) {
     var r = s.split("").reverse().join("");
     return r != n && isPrime(n) && isPrime(r);
 }
- 
-function main() {
-	// 5
-    var one = document.getElementById("5");
-    var two = document.getElementById("15");
- 
-    var c = 0;
-    var x = 11;
-    var str;
- 
-    while (c < 79) {
-        if (isEmirp(x)) {
-            c += 1;
- 
-            // 5
-            if (c == 1) {
-                str = "<h3>5</h3>" + x + " " + "and" + " " + x + " " + "are Match";
-            }
-            else if (c < 4) {
-                str += "<br/>" + x + " " + "and" + " " + x + " " + "are Match";
-            }
-            else if (c == 4) {
-                one.innerHTML = str + "<br/>" + x + " " + "and" + " " + x + " " + "are Match<br/><br/>";
-            }
 
-            // 15
-            else if (c == 1) {
-                str = "<h3>15</h3>" + x + " " + "and" + " " + x + " " + "are Match";
-            }
-            else if (c < 10) {
-                str += "<br/>" + x + " " + "and" + " " + x + " " + "are Match";
-            }
-            else if (c == 10) {
-                two.innerHTML = str + "<br/>" + x + " " + "and" + " " + x + " " + "are Match<br/><br/>";
-            }
+// Urutkan Abjad
+// function urutkanAbjad(str) {
+//   return str.toString().split('').sort().join('');
+// }
+ 
+
+// 5
+var one = document.getElementById("5");
+var two = document.getElementById("15");
+
+var c = 0;
+var x = 11;
+var str;
+
+while (c < 79) {
+    if (isEmirp(x)) {
+        c += 1;
+
+        // 5
+        if (c == 1) {
+            str = "<h3>5</h3>" + x + " " + "and" + " " + x + " " + "are Match";
         }
-        x += 2;
+        else if (c < 4) {
+            str += "<br/>" + x + " " + "and" + " " + x + " " + "are Match";
+        }
+        else if (c == 4) {
+            one.innerHTML = str + "<br/>" + x + " " + "and" + " " + x + " " + "are Match<br/><br/>";
+        }
+
+        // 15
+        else if (c == 1) {
+            str = "<h3>15</h3>" + x + " " + "and" + " " + x + " " + "are Match";
+        }
+        else if (c < 10) {
+            str += "<br/>" + x + " " + "and" + " " + x + " " + "are Match";
+        }
+        else if (c == 10) {
+            two.innerHTML = str + "<br/>" + x + " " + "and" + " " + x + " " + "are Match<br/><br/>";
+        }
     }
+    x += 2;
 }
